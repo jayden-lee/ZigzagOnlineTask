c9e0d1cebdac7c4263abb124636ce282ae15ef12
Jayden <jayden@chequer.io>
2019-12-29 12:10:00 +0900

    add testcase

--- FunctionTask/src/test/java/date/DateFormatMainTest.java
+++ FunctionTask/src/test/java/date/DateFormatMainTest.java
@@ -23,6 +23,21 @@ public class DateFormatMainTest {
     }
 
     @Test
+    @DisplayName("AM 시간 값에 N초 후 정오값이 출력되는지 검증하는 테스트")
+    public void Add_AmDate_AddSeconds_Return_Noontime() {
+        // given
+        String dateString = "AM 11:59:59";
+        int second = 1;
+
+        // when
+        CustomTime customTime = new CustomTime(dateString);
+        customTime = customTime.plusSecond(second);
+
+        // then
+        assertEquals(customTime.formatDateString(), "12:00:00");
+    }
+
+    @Test
     @DisplayName("PM 시간 값에 N초 후 출력값이 올바른지 검증하는 테스트")
     public void Add_PmDate_AddSeconds() {
         // given
@@ -35,6 +50,21 @@ public class DateFormatMainTest {
 
         // then
         assertEquals(customTime.formatDateString(), "13:00:10");
+    }
+
+    @Test
+    @DisplayName("PM 시간 값에 N초 후 자정값이 출력되는지 검증하는 테스트")
+    public void Add_PmDate_AddSeconds_Return_Midnight() {
+        // given
+        String dateString = "PM 11:59:59";
+        int second = 1;
+
+        // when
+        CustomTime customTime = new CustomTime(dateString);
+        customTime = customTime.plusSecond(second);
+
+        // then
+        assertEquals(customTime.formatDateString(), "00:00:00");
     }
 
     @Test
